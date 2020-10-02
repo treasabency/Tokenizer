1024b1b4f414ed405fb1b4681d7f1efbded948d0
Sagnik M <sagnim1337@gmail.com>
2020-10-02 01:02:25 -0400

    Update Tokenizer.c
    -Changed overall program logic by incrementing through string pointer rather than delimiting string on whitespace into substrings
    -Added token recognition for C operators as outlined in Refcard.txt
    NOTE: I know the switch-case block for C operators is beyond overkill, but I really couldn't think of a way to print the appropriate message for over forty unique special characters and check each individual character otherwise; it would be easier with a hash table, but that would require another function entirely and would complicate things needlessly, and each operator token is unique with its own name message anyway, so it would not save that many lines of code

--- Tokenizer.c
+++ Tokenizer.c
@@ -18,11 +18,11 @@ int main()
     limit = 30;
      
     //allocate memory dynamically
-    test=(char*)malloc(limit*sizeof(char));
+    test = (char*)malloc(limit*sizeof(char));
     
     //prompt and store input 
     printf("Enter string: ");
-    scanf("%[^\n]",test);
+    gets(test);
 
     //end early if string empty or purely whitespace
     if(isEmpty(test) == 1)
@@ -61,105 +61,434 @@ int isEmpty(char* str)
 }
 
 //helper function, recognize and print tokens
-void tokenize (char* str) 
+void tokenize (char* str)
 {
-	//tokenArr is separate from user input,
-	//input string remains unchanged afterwards
-    char tokenArr[strlen(str)][strlen(str)]; 
-
-    //i = rows for each distinct token, 
-    //j = columns for each token character
-    int i, j, numTokens;
-    j = 0, numTokens = 0;
-
-    //any whitespace in input string
-    //becomes null operator in char array
-    for(i = 0; i <= (strlen(str)); i++)
-    {
-    	//any whitespace or null operator
-    	//signals the end of a token
-        if(str[i] == ' ' || str[i] == '\0' 
-        	|| str[i] == '\t' || str[i] == '\v'
-        	|| str[i] == '\f' || str[i] == '\n'
-        	|| str[i] == '\r')
-        {
-            tokenArr[numTokens][j] = '\0';
-            numTokens++;
-            j = 0;
-        }
-        //store entire token into a row
-        //in char array
-        else
-        {
-            tokenArr[numTokens][j] = str[i];
-            j++;
-        }
-    }
-
     //breaking tokens into specific types
     //then displaying categorically
-    for(i = 0; i < numTokens; i++) 
+    //until end of pointer reached
+    while(*str)
     {
     	//word: alphabetic char, followed by 
     	//alphanumeric char(s)
-		if(isalpha(tokenArr[i][0]))
+		if(isalpha(*str))
 		{
-			printf("word: ");
-			j = 0;
-			while(isalnum(tokenArr[i][j]))
+			if(*str == 's'
+				&& *(str + 1) == 'i'
+				&& *(str + 2) == 'z'
+				&& *(str + 3) == 'e'
+				&& *(str + 4) == 'o'
+				&& *(str + 5) == 'f')
 			{
-				printf("%c", tokenArr[i][j]);
-				j++;
+				printf("sizeof: sizeof\n");
+				*str += 6;
+			}
+
+			printf("word: ");
+			while(isalnum(*str))
+			{
+				printf("%c", *str);
+				*str++;
 			}
 			printf("\n");
 		}
 
 		//hexadecimal integer: 0x or 0X, followed by
 		//[0-9][a-f][A-F] char(s)
-		else if(tokenArr[i][0] == '0' && tokenArr[i][1] == 'x'
-			|| tokenArr[i][0] == '0' && tokenArr[i][1] == 'X') 
+		else if(*str == '0' && *str == 'x'
+			|| *str == '0' && *str == 'X') 
 		{
 			printf("hexadecimal integer: ");
-			printf("%c", tokenArr[i][0]);
-			printf("%c", tokenArr[i][1]);
-			j = 2;
-			while(isxdigit(tokenArr[i][j]))
+			printf("%c", *str);
+			printf("%c", *str);
+			while(isxdigit(*str))
 			{
-				printf("%c", tokenArr[i][j]);
-				j++;
+				printf("%c", *str);
+				*str++;
 			}
 			printf("\n");
 		}
 
 		//octal integer: 0, followed by
 		//[0-7] char(s)
-		else if(tokenArr[i][0] == '0') 
+		else if(*str == '0') 
 		{
 			printf("octal integer: ");
-			j = 0;
-			while(tokenArr[i][j] >= '0' && tokenArr[i][j] <= '7')
+			while(*str >= '0' && *str <= '7')
 			{
-				printf("%c", tokenArr[i][j]);
-				j++;
+				printf("%c", *str);
+				*str++;
 			}
 			printf("\n");
 		}
 
 		//decimal integer: digit char, followed by
 		//digit char(s)
-		else if(isdigit(tokenArr[i][0])) 
+		else if(isdigit(*str)) 
 		{
 			printf("decimal integer: ");
-			j = 0;
-			while(isdigit(tokenArr[i][j]))
+			while(isdigit(*str))
 			{
-				printf("%c", tokenArr[i][j]);
-				j++;
+				printf("%c", *str);
+				*str++;
 			}
 			printf("\n");
 		}
 
-		else 
-		 	printf("");
+		//C operators
+		else if(isprint(*str))
+		{
+			switch(*str) 
+			{
+			   	case '(':
+					printf("left parenthesis: ");
+					printf("%c\n", *str);
+					*str++;
+					break;
+				
+			   	case ')':
+					printf("right parenthesis: ");
+					printf("%c\n", *str);
+					*str++;
+					break;
+			  
+				case '[':
+			     	printf("left bracket: ");
+			     	printf("%c\n", *str);
+			     	*str++;
+			     	break;
+
+				case ']':
+			     	printf("right bracket: ");
+			     	printf("%c\n", *str);
+			     	*str++;
+			     	break;
+
+				case '.':
+			     	printf("structure member: ");
+			     	printf("%c\n", *str);
+			     	*str++;
+			     	break;
+
+				case '-':
+					switch(*(str + 1))
+					{
+						case '>':
+							printf("structure pointer: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						case '-':
+							printf("decrement: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						case '=':
+							printf("minus equals: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						default:
+							printf("minus/subtract operator: ");
+							printf("%c\n", *str);
+							*str++;
+					}
+					break;
+
+				case ',':
+			     	printf("comma: ");
+			     	printf("%c\n", *str);
+			     	*str++;
+			     	break;
+
+			    case '!':
+			    	if(*(str + 1) == '=')
+			    	{
+						printf("inequality test: ");
+						printf("%c", *str);
+						*str++;
+						printf("%c\n", *str);
+						*str++;
+					}
+					else
+					{
+				     	printf("negate: ");
+				     	printf("%c\n", *str);
+				     	*str++;
+				    }
+			     	break;
+
+			    case '~':
+			     	printf("1s complement: ");
+			     	printf("%c\n", *str);
+			     	*str++;
+			     	break;
+
+			    case '>':
+			     	switch(*(str + 1))
+					{
+						case '>':
+							if(*(str + 2) == '=')
+							{
+								printf("shift right equals: ");
+								printf("%c", *str);
+								*str++;
+								printf("%c", *str);
+								*str++;
+								printf("%c\n", *str);
+								*str++;
+							}
+							else
+							{
+								printf("shift right: ");
+								printf("%c", *str);
+								*str++;
+								printf("%c\n", *str);
+								*str++;
+							}
+							break;
+
+						case '=':
+							printf("greater than or equal test: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						default:
+							printf("greater than test: ");
+							printf("%c\n", *str);
+							*str++;
+					}
+					break;
+
+			    case '<':
+			     	switch(*(str + 1))
+					{
+						case '<':
+							if(*(str + 2) == '=')
+							{
+								printf("shift left equals: ");
+								printf("%c", *str);
+								*str++;
+								printf("%c", *str);
+								*str++;
+								printf("%c\n", *str);
+								*str++;
+							}
+							else
+							{
+								printf("shift left: ");
+								printf("%c", *str);
+								*str++;
+								printf("%c\n", *str);
+								*str++;
+							}
+							break;
+
+						case '=':
+							printf("less than or equal test: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						default:
+							printf("less than test: ");
+							printf("%c\n", *str);
+							*str++;
+					}
+					break;
+
+				case '^':
+			    	if(*(str + 1) == '=')
+			    	{
+						printf("bitwise XOR equals: ");
+						printf("%c", *str);
+						*str++;
+						printf("%c\n", *str);
+						*str++;
+					}
+					else
+					{
+				     	printf("bitwise XOR: ");
+				     	printf("%c\n", *str);
+				     	*str++;
+				    }
+			     	break;
+
+				case '|':
+					switch(*(str + 1))
+					{
+						case '|':
+							printf("logical OR: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						case '=':
+							printf("bitwise OR equals: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						default:
+							printf("bitwise OR: ");
+							printf("%c\n", *str);
+							*str++;
+					}
+					break;
+
+				case '+':
+					switch(*(str + 1))
+					{
+						case '+':
+							printf("increment: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						case '=':
+							printf("plus equals: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						default:
+							printf("addition: ");
+							printf("%c\n", *str);
+							*str++;
+					}
+					break;
+
+				case '/':
+			    	if(*(str + 1) == '=')
+			    	{
+						printf("divide equals: ");
+						printf("%c", *str);
+						*str++;
+						printf("%c\n", *str);
+						*str++;
+					}
+					else
+					{
+				     	printf("division: ");
+				     	printf("%c\n", *str);
+				     	*str++;
+				    }
+			     	break;
+
+				case '*':
+			    	if(*(str + 1) == '=')
+			    	{
+						printf("times equals: ");
+						printf("%c", *str);
+						*str++;
+						printf("%c\n", *str);
+						*str++;
+					}
+					else
+					{
+				     	printf("multiply/dereference operator: ");
+				     	printf("%c\n", *str);
+				     	*str++;
+				    }
+			     	break;
+
+			    case '=':
+			    	if(*(str + 1) == '=')
+			    	{
+						printf("equality test: ");
+						printf("%c", *str);
+						*str++;
+						printf("%c\n", *str);
+						*str++;
+					}
+					else
+					{
+				     	printf("assignment: ");
+				     	printf("%c\n", *str);
+				     	*str++;
+				    }
+			     	break;
+
+			    case '&':
+					switch(*(str + 1))
+					{
+						case '&':
+							printf("logical AND: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						case '=':
+							printf("bitwise AND equals: ");
+							printf("%c", *str);
+							*str++;
+							printf("%c\n", *str);
+							*str++;
+							break;
+
+						default:
+							printf("AND/address operator: ");
+							printf("%c\n", *str);
+							*str++;
+					}
+					break;
+
+			    case '%':
+			    	if(*(str + 1) == '=')
+			    	{
+						printf("mod equals: ");
+						printf("%c", *str);
+						*str++;
+						printf("%c\n", *str);
+						*str++;
+					}
+					else
+					{
+				     	printf("modulus/remainder: ");
+				     	printf("%c\n", *str);
+				     	*str++;
+				    }
+			     	break;
+
+				case '?':
+			     	printf("conditional true: ");
+			     	printf("%c\n", *str);
+			     	*str++;
+			     	break;
+
+				case ':':
+			     	printf("conditional false: ");
+			     	printf("%c\n", *str);
+			     	*str++;
+			     	break;
+
+			    default :
+			   		*str++;
+			}
+
+		}
 	}
 }
